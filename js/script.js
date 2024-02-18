be6e331325c6a6d831aad45d866f5b4d6a49f311
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-02-18 12:20:58 +0600

    functionality adding

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
-function changeColor(event) {
+const availableSeat=40;
+const seatBookingCount=0;
+const oneTicketPrice=550;
+const totalPrice=seatBookingCount*oneTicketPrice;
+const grandTotal=seatBookingCount*totalPrice;
+
+function selectingToBuying(event) {
   const element = event.target; 
   console.log(event.target.id);
   element.classList.add('bg-[#1DD100]');
+
+ 
+  
 }
 
-function removeBackgroundColor(event){
+function removeSelection(event){
   const element = event.target;
   element.classList.remove('bg-[#1DD100]');
 
 }
+function getValueById(elementId) {
+ const element= document.getElementById(elementId);
+ const elementText=element.value ;
+ const elementValue=parseInt(elementText);
+ return elementValue;
+}
+
+function buyingTicket() {
+ 
+}
+document.getElementById('phone-number').addEventListener('keyup',function (event) {
+  const number=event.target.value;
+  const stringNumber=String(number);
+  const numberLength=stringNumber.length;
+  const nextButton=document.getElementById('next')
+  if (numberLength>0) {
+      nextButton.removeAttribute('disabled')
+  }else{
+      nextButton.setAttribute('disabled',true)
+  }
+
+})
